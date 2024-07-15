b6684f7187e9e334eee9708d29c5821ccdc575fe
Alex Hübner <alexhbnr@gmail.com>
2024-07-15 14:31:09 +0200

    Change formatting

--- lua/plugins/nvim-r.lua
+++ lua/plugins/nvim-r.lua
@@ -1,13 +1,13 @@
 return {
-  "jalvesaq/Nvim-R",
-  name = "nvim-r",
+  'jalvesaq/Nvim-R',
+  name = 'nvim-r',
   enabled = true,
   lazy = false,
   init = function()
     vim.g.R_openpdf = 1
     vim.g.R_clear_console = 0
     vim.g.R_rconsole_width = 0
-    vim.cmd([[
+    vim.cmd [[
       " your Nvim-R configuration goes here, e.g:
       let R_hl_term = 0
       let R_bracketed_paste = 1
@@ -16,6 +16,6 @@ return {
 
       map <silent> \xh :call RAction("head")<CR>
       map <silent> \xo :call RAction("class")<CR>
-    ]])
+    ]]
   end,
 }
